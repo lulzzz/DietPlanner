a4fd9ff579d8ac03986151636e2c33bd171536ab
Michal Borowski <mboroo@gmail.com>
2016-11-06 15:00:50 +0100

    Add Radnom Recipe generator

--- ConsoleInterface/Program.cs
+++ ConsoleInterface/Program.cs
@@ -4,8 +4,8 @@ using System.Linq;
 using DietPlanning.Core;
 using DietPlanning.Core.DataProviders.Databse;
 using DietPlanning.Genetic;
-using DietPlanning.Genetic.CrossingOver;
-using DietPlanning.Genetic.Selectors;
+using DietPlanning.NSGA;
+using Random = System.Random;
 
 namespace ConsoleInterface
 {
@@ -13,28 +13,15 @@ namespace ConsoleInterface
   {
     public static void Main(string[] args)
     {
-      const int gaPopulationSize = 100;
-      const int gaMaxIterations = 10000;
-      const double gaMutationProbability = 0.2;
-      const double gaSelectFactor = 0.95;
-      const int numberOfMealsPerDay = 5;
-      const int numberOfDays = 7;
-      const int tournamentSize = 10;
+      var solver = new NsgaSolver(new Sorter());
 
-      var random = new Random();
-      var dietAnalyzer = new DietAnalyzer();
-      var gaConfiguration = new Configuration(gaMutationProbability, gaSelectFactor, numberOfDays, numberOfMealsPerDay, gaPopulationSize, gaMaxIterations);
-      var selector = new TournamentSelector(tournamentSize, random);
-      var geneticDietSolver = new GeneticDietSolver(new PopulationInitializer(random), selector, new DaysCrossOver(random), new Evaluator(new DietAnalyzer()));
+      var recipeGenerator = new RandomRecipeProvider(new Random());
 
-      var foodsProvider = new FoodDatabaseProvider();
-      
-      var diets = geneticDietSolver.GetDiet(foodsProvider.GetFoods(), GetTargetDiet(), gaConfiguration);
+      var recipes = recipeGenerator.GetRecipes(100, new FoodDatabaseProvider().GetFoods());
 
-      var summaries = diets.Select(dietAnalyzer.Summarize).ToList();
-      Presenter.Output(diets);
-      Console.WriteLine(summaries.Count);
-     // Console.Read();
+      Console.WriteLine(recipes.Count);
+
+      Console.ReadKey();
     }
 
     private static DietSummary GetTargetDiet()
---
+++ DietPlanning.Core/DataProviders/Databse/RandomRecipeProvider.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DietPlanning.Core.DomainObjects;
+
+namespace DietPlanning.Core.DataProviders.Databse
+{
+  public class RandomRecipeProvider
+  {
+    private readonly Random _random;
+
+    public RandomRecipeProvider(Random random)
+    {
+      _random = random;
+    }
+
+    public List<Recipe> GetRecipes(int size, List<Food> foods)
+    {
+      var recipes = new List<Recipe>();
+
+      for (var i = 0; i < size; i++)
+      {
+        recipes.Add(GetRandomRecipe(foods));
+      }
+
+      return recipes;
+    }
+
+    private Recipe GetRandomRecipe(List<Food> foods)
+    {
+      var receipeGroups = Enum.GetValues(typeof(RecipeGroup));
+
+      var recipe = new Recipe
+      {
+        Cost = _random.Next(5, 35),
+        PreparationTimeInMinutes = _random.Next(1, 10)*10,
+        Group = (RecipeGroup) receipeGroups.GetValue(_random.Next(receipeGroups.Length)),
+        Ingredients = GetRandomFoods(foods)
+      };
+
+      return recipe;
+    }
+
+    private List<FoodPortion> GetRandomFoods(List<Food> foods)
+    {
+      var numberOfIngredients = _random.Next(2, 6);
+      var foodPortions = new List<FoodPortion>();
+
+      for (var i = 0; i < numberOfIngredients; i++)
+      {
+        Food food;
+        do
+        {
+          food = foods[_random.Next(foods.Count)];
+        } while (foodPortions.Any(portion => portion.Food == food));
+
+        foodPortions.Add(new FoodPortion(food, _random.Next(10, 150)));
+      }
+
+      return foodPortions;
+    }
+  }
+}
