a6375101fa00dbb0ba68b6c163778288ec520a0e
Michal Borowski <mboroo@gmail.com>
2016-11-27 15:18:08 +0100

    Add local publish profile

--- DietPlanning.Web/App_Start/BundleConfig.cs
+++ DietPlanning.Web/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace DietPlanning.Web
     public static void RegisterBundles(BundleCollection bundles)
     {
       bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                  "~/Scripts/jquery-{version}.js"));
+                  "~/Scripts/jquery-{version}.js",
+                  "~/Scripts/jquery-ui.min.js"));
 
       bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                   "~/Scripts/jquery.validate*"));
@@ -28,12 +29,8 @@ namespace DietPlanning.Web
 
       bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/bootstrap.css",
+                "~/Content/jquery-ui.min.css",
                 "~/Content/site.css"));
-
-      bundles.Add(new ScriptBundle("~/bundles/jquery-ui").Include(
-        "~/Scripts/jquery-ui.min.js"));
-      bundles.Add(new StyleBundle("~/bundles/jquery-ui.css").Include(
-         "~/Content/jquery-ui.min.css"));
     }
   }
 }
